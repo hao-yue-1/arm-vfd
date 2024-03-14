31693a01b629d5195eb6572963b2393728d62c61
hao-yue-1 <1801134420@qq.com>
2024-03-14 21:44:37 +0800

    init lcd and ec11

---
+++ program/Hardware/debug.h
@@ -0,0 +1,12 @@
+//
+// Created by yue on 2023/12/11.
+//
+
+#ifndef EC11_F103_DEBUG_H
+#define EC11_F103_DEBUG_H
+
+#include <stdio.h>
+
+#define DEBUG_ERROR() printf("[ERROR] function is %s | line is %d\r\n", __FUNCTION__, __LINE__)
+
+#endif //EC11_F103_DEBUG_H
---
+++ program/Hardware/lcd_font.h
@@ -0,0 +1,27 @@
+//
+// Created by yue on 2023/12/8.
+//
+
+#ifndef _LCDFONT_H
+#define _LCDFONT_H
+
+#include <stdint.h>
+
+extern const uint8_t asc2_1608[1520];
+
+/* ÊÖĞ´Êı×Ö 0-9 80*80 */
+extern const uint8_t bin_0[1608];
+extern const uint8_t bin_1[1608];
+extern const uint8_t bin_2[1608];
+extern const uint8_t bin_3[1608];
+extern const uint8_t bin_4[1608];
+extern const uint8_t bin_5[1608];
+extern const uint8_t bin_6[1608];
+extern const uint8_t bin_7[1608];
+extern const uint8_t bin_8[1608];
+extern const uint8_t bin_9[1608];
+
+extern const uint8_t img_cha[6058];
+extern const uint8_t img_han[6058];
+
+#endif
