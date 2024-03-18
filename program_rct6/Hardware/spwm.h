1a97c9e5166e950219b9fb42344959c1654efe6e
hao-yue-1 <1801134420@qq.com>
2024-03-18 18:13:43 +0800

    add rct6 version for testing

---
+++ program_rct6/Hardware/spwm.h
@@ -0,0 +1,13 @@
+//
+// Created by yue on 2024/3/18.
+//
+
+#ifndef PROGRAM_RCT6_SPWM_H
+#define PROGRAM_RCT6_SPWM_H
+
+#include <stdint.h>
+
+#define SPWM_50 2000
+extern uint16_t spwm_list_50[SPWM_50];
+
+#endif //PROGRAM_RCT6_SPWM_H
